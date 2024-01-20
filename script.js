f0ba8565befacc97fedcd10ef4137688d2ec4cfb
Alex Chang <148062152+inch6134@users.noreply.github.com>
2024-01-19 16:13:31 -0800

    Build playRound function and test

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-function getComputerChoice(remainder) {
-    switch(remainder) {
-        case 0:
-            return "Rock";
-        
-        case 1:
-            return "Paper";
+function getComputerChoice() {
+  let remainder = Math.floor(Math.random() * 100) % 3;
+  switch (remainder) {
+    case 0:
+      return "rock";
 
-        case 2:
-            return "Scissors";
-     }
+    case 1:
+      return "paper";
+
+    case 2:
+      return "scissors";
+  }
 }
 
-function calcRemainder() {
-    return Math.floor(Math.random() * 100) % 3;
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase();
+  console.log(player);
+  let computer = computerSelection;
+  console.log(computer);
+
+  if (player === computer) {
+    return `Tie!`;
+  } else if (
+    (player === "rock" && computer === "paper") ||
+    (player === "paper" && computer === "scissors") ||
+    (player === "scissors" && computer === "rock")
+  ) {
+    return `You lose! ${computer} beats ${player}`;
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    return `You win! ${player} beats ${computer}`;
+  }
 }
 
-console.log(calcRemainder());
-console.log(getComputerChoice(calcRemainder()));
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
