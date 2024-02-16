c1a8ab4f6a38d6536268a52cc7d07c23f7226c24
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-02-15 19:29:02 -0800

    implement global state in game() and displaySelections() utility function

--- script.js
+++ script.js
@@ -1,83 +1,96 @@
-// Global variables
-
-  let playerWinCounter = 0;
-  let computerWinCounter = 0;
-  let gameCounter = 1;
+// Global state
+let state = {};
+ 
 
 // references to HTML elements
 
-const rockButton = document.querySelector("#rock")
-const paperButton = document.querySelector("#paper");
-const scissorsButton = document.querySelector("#scissors");
+const rockButtonDOM = document.querySelector("#rock");
+const paperButtonDOM = document.querySelector("#paper");
+const scissorsButtonDOM = document.querySelector("#scissors");
+const selectionsDOM = document.querySelector("#selections");
 
 // Event listeners for buttons
 
-rockButton.addEventListener("click", ()=>
-  playRound("rock", getComputerChoice)
-);
+rockButtonDOM.addEventListener("click", ()=> {
+  state.playerSelection = "rock";
+  state.computerSelection = getComputerChoice();
+  state.outcome = playRound(state.playerSelection, state.computerSelection);
+  displaySelections();
+});
 
-paperButton.addEventListener("click", ()=>
-  playRound("paper", getComputerChoice)
-);
+paperButtonDOM.addEventListener("click", ()=> {
+  state.playerSelection = "paper";
+  state.computerSelection = getComputerChoice();
+  state.outcome = playRound(state.playerSelection, state.computerSelection);
+  displaySelections();
+});
 
-scissorsButton.addEventListener("click", ()=>
-  playRound("scissors", getComputerChoice)
-);
+scissorsButtonDOM.addEventListener("click", ()=> {
+  state.playerSelection = "scissors";
+  state.computerSelection = getComputerChoice();
+  state.outcome = playRound(state.playerSelection, state.computerSelection);
+  displaySelections();
+});
 
 game();
 
 // main function
 
 function game() {
-  
-  do {
-    const playerSelection = prompt("Rock Paper Scissors!");
-    const computerSelection = getComputerChoice();
-    const outcome = playRound(playerSelection, computerSelection);
-    console.log(`Player chose ${playerSelection}!`);
-    console.log(`Computer chose ${computerSelection}!`);
-    console.log(outcome);
+  state = {
+    playerWinCounter: 0,
+    computerWinCounter: 0,
+    gameCounter: 1,
+    playerSelection: "",
+    computerSelection: "",
+    outcome: "",
+  };
 
-    if (/win/.test(outcome)) {
-      playerWinCounter++;
-      console.log(`Round ${gameCounter}: Player (${playerWinCounter} wins)`);
-      gameCounter++;
-    } else if (/lose/.test(outcome)) {
-      computerWinCounter++;
-      console.log(
-        `Round ${gameCounter}: Computer (${computerWinCounter} wins)`
-      );
-      gameCounter++;
-    }
-  } while (playerWinCounter < 3 && computerWinCounter < 3 && gameCounter <= 5);
+  // reset HTML elements
+  selectionsDOM.innerHTML = "";
 
-  if (playerWinCounter === 3) {
-    console.log("Congrats! You Win! Final Score: ");
-    console.log(`Player: ${playerWinCounter}`);
-    console.log(`Computer: ${computerWinCounter}`);
-  } else if (computerWinCounter === 3) {
-    console.log("Bummer! You Lost! Final Score: ");
-    console.log(`Player: ${playerWinCounter}`);
-    console.log(`Computer: ${computerWinCounter}`);
-  }
+  // do {
+    
+    // move logic to new utility function, call in event listeners
+    // store computerSelection in global variable to call in multiple places
+    // add single line that displays message of current selections
+
+    // const playerSelection = prompt("Rock Paper Scissors!");
+    // const computerSelection = getComputerChoice();
+    // const outcome = playRound(playerSelection, computerSelection);
+    // console.log(`Player chose ${playerSelection}!`);
+    // console.log(`Computer chose ${computerSelection}!`);
+    // console.log(outcome);
+
+
+    // utility function - countRound()
+  //   if (/win/.test(outcome)) {
+  //     playerWinCounter++;
+  //     console.log(`Round ${gameCounter}: Player (${playerWinCounter} wins)`);
+  //     gameCounter++;
+  //   } else if (/lose/.test(outcome)) {
+  //     computerWinCounter++;
+  //     console.log(
+  //       `Round ${gameCounter}: Computer (${computerWinCounter} wins)`
+  //     );
+  //     gameCounter++;
+  //   }
+//  } while (state.playerWinCounter < 3 && computerWinCounter < 3 && gameCounter <= 5);
+
+
+  // utility function - keepScore()
+//   if (playerWinCounter === 3) {
+//     console.log("Congrats! You Win! Final Score: ");
+//     console.log(`Player: ${playerWinCounter}`);
+//     console.log(`Computer: ${computerWinCounter}`);
+//   } else if (computerWinCounter === 3) {
+//     console.log("Bummer! You Lost! Final Score: ");
+//     console.log(`Player: ${playerWinCounter}`);
+//     console.log(`Computer: ${computerWinCounter}`);
+//   }
 }
 
 // Utility functions
-
-function getComputerChoice() {
-  let remainder = Math.floor(Math.random() * 100) % 3;
-  switch (remainder) {
-    case 0:
-      return "rock";
-
-    case 1:
-      return "paper";
-
-    case 2:
-      return "scissors";
-  }
-}
-
 
 function playRound(playerSelection, computerSelection) {
 
@@ -98,4 +111,24 @@ function playRound(playerSelection, computerSelection) {
   } else {
     return "Please enter a valid hand.";
   }
+}
+
+function getComputerChoice() {
+  let remainder = Math.floor(Math.random() * 100) % 3;
+  switch (remainder) {
+    case 0:
+      return "rock";
+
+    case 1:
+      return "paper";
+
+    case 2:
+      return "scissors";
+  }
+}
+
+
+function displaySelections() {
+  selectionsDOM.innerHTML = `Player chose ${state.playerSelection}!\n
+  Computer chose ${state.computerSelection}!`;
 }
